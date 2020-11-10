73589e72159a2588bed98fa5f0ffed160ce583f6
akhilmhdh <akhilmhdh@gmail.com>
2020-11-10 23:25:46 +0530

    added env config load

--- index.ts
+++ index.ts
@@ -1,10 +1,14 @@
 import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
-const PORT = 8000
 
 app.get('/', (req, res) => res.send('Express + TypeScript Server'))
 
+const PORT = process.env.PORT || 8000
+
 app.listen(PORT, () => {
-    console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`)
+    console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`)
 })
