589f3f67d823c71643809f307f8351fbef8683e7
akhilmhdh <akhilmhdh@gmail.com>
2020-11-11 20:23:26 +0530

    added logger

---
+++ components/utils/logger.ts
@@ -0,0 +1,22 @@
+import pino, { LoggerOptions } from 'pino'
+// import path from 'path'
+const logLevels = ['fatal', 'error', 'warn', 'info', 'debug', 'trace']
+
+const getLogLevel = () => {
+    const givenLevel = logLevels.indexOf(process.env.LOG_LEVEL || 'debug')
+    return logLevels[givenLevel]
+}
+
+const getBool = (env: undefined | string) => {
+    if (!env) return true
+    return env.toLowerCase() === 'true' ? true : false
+}
+
+const loggerConfig: LoggerOptions = {
+    level: getLogLevel(),
+    timestamp: getBool(process.env.LOG_TIME_STAMP),
+    prettyPrint: {},
+    prettifier: require('pino-colada'),
+}
+//  path.resolve(__dirname) + "/../logs/info.log")
+export default pino(loggerConfig)
--- index.ts
+++ index.ts
@@ -1,9 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import expressPinoLogger from 'express-pino-logger'
+import logger from './components/utils/logger'
 
 dotenv.config()
 
 const app = express()
+app.use(expressPinoLogger({ logger: logger }))
 
 app.get('/', (req, res) => res.send('Express + TypeScript Server'))
 
