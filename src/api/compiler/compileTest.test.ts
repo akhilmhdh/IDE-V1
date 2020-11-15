81754a98d1b1c2f383c6e230f1b49a0e02891dfe
akhilmhdh <akhilmhdh@gmail.com>
2020-11-15 19:34:45 +0530

    setup for test complete

---
+++ src/api/compiler/compileTest.test.ts
@@ -0,0 +1,16 @@
+/**
+ * importing testing libraries
+ */
+import { expect } from 'chai';
+import { it, describe } from 'mocha';
+
+/**
+ * testing parts
+ */
+import CodeRunner from './codeRunner';
+
+describe('c program runner', () => {
+    it('should run the c program', () => {
+        expect(true).to.be.true;
+    });
+});
